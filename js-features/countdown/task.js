72f62de2e2bb7a117084009362aa5819f6e40f0a
Ishchenko Evgenii <Evgenii.Ishchenko@yandex.ru>
2024-02-20 21:32:33 +0300

    first commit countdown

---
+++ js-features/countdown/task.js
@@ -0,0 +1,14 @@
+"use strict";
+
+const timerReduce = function() {
+    const timer = document.getElementById('timer');
+    timer.textContent -= 1;
+
+    if (timer.textContent === '0') {
+        alert('Вы победили в конкурсе!')
+        clearInterval(intervalId);
+    }
+
+}
+
+const intervalId = setInterval(timerReduce, 100);
