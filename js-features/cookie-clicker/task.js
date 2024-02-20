b1d70c59ce47f046a8f0b1a3af6b52ab6a281a5c
Ishchenko Evgenii <Evgenii.Ishchenko@yandex.ru>
2024-02-20 21:38:41 +0300

    first commit cookie-clicker

---
+++ js-features/cookie-clicker/task.js
@@ -0,0 +1,16 @@
+"use strict";
+
+const counter = document.getElementById('clicker__counter');
+
+const cookie = document.getElementById('cookie');
+
+cookie.onclick = function() {
+    counter.textContent ++
+    cookie.width = 220;
+    cookie.height = 160;
+    if (!(counter.textContent % 2)) {
+      cookie.width = 200;
+      cookie.height = 129;
+    }
+}
+
