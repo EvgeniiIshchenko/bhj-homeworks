d6b7e9b54c9fb6c89110be17d21221e286c506f3
Ishchenko Evgenii <Evgenii.Ishchenko@yandex.ru>
2024-04-15 17:22:56 +0300

    first commit text-editor

---
+++ client-state/text-editor/task.js
@@ -0,0 +1,10 @@
+"use strict";
+
+const editor = document.getElementById('editor');
+const storedText = localStorage.getItem('text')
+
+editor.value = storedText;
+
+editor.addEventListener('input', () => {
+    localStorage.setItem('text', editor.value);
+})
