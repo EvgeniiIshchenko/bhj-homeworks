3005b9d6c397eaf3c406bbf380b99d17d814529b
Ishchenko Evgenii <Evgenii.Ishchenko@yandex.ru>
2024-04-15 09:55:47 +0300

    second commit progressbar

--- async-requests/progressbar/task.js
+++ async-requests/progressbar/task.js
@@ -8,22 +8,10 @@ form.addEventListener("submit", (e) => {
 
   const xhr = new XMLHttpRequest();
 
-  xhr.addEventListener("readystatechange", () => {
-    switch (xhr.readyState) {
-      case xhr.OPENED:
-        progress.value = 0.25;
-        break;
-      case xhr.HEADERS_RECEIVED:
-        progress.value = 0.5;
-        break;
-      case xhr.LOADING:
-        progress.value = 0.75;
-        break;
-      case xhr.DONE:
-        progress.value = 1;
-        break;
-    }
-  });
+  xhr.upload.onprogress = function (event) {
+    const { loaded, total } = event;
+    progress.value = Math.round(loaded / total);
+  };
 
   xhr.open("POST", "https://students.netoservices.ru/nestjs-backend/upload");
   const formData = new FormData(form);
